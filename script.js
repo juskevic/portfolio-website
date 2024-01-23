7f789594d8568e59583c81f4fa50828d2e5e5d39
kituccino <73663894+kituccino@users.noreply.github.com>
2024-01-23 15:05:34 +0100

    ADDED SOME JAVASCRIPT!!!!

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let aboutMeText = document.getElementById('about-me-text');
+let originalText = aboutMeText.innerText;
+let newText = "Hi thewe!!11 *cries* My nyame is Max, and I-I-I’m a web devewopew *blushes* who is passionyate about c-cweating beautifuw and functionyaw websites. I-I-I’m cuwwentky UwU weawnying JavaScwipt and Awgowithms t-to enhance my skiwws and knyowwedge. I-I-I’m awso pwoficient *sweats* in H-HTMW and CSS. *looks at you* In the x3 futuwe, I-I-I’m wooking fowward t-to weawneding mroe fwont-end technyowogies t-to cweate even OwO mowe amazing websites!!11 😊 (Wwitten on 23/01/2024)";
 
+aboutMeText.addEventListener('click', function() {
+    if (aboutMeText.innerText === originalText) {
+        aboutMeText.innerText = newText; // Change text
+    } else {
+        aboutMeText.innerText = originalText; // Change text back
+    }
+});
