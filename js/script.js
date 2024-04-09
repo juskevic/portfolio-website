a38338ec79396754b79041cb681f76dac92d342f
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-04-09 23:13:47 +0200

    Moved "under construction banner" from styles to index for better readily.

--- js/script.js
+++ js/script.js
@@ -34,15 +34,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
     switchInput.addEventListener('change', toggleNightMode);
 });
-
-/* Website under construction bar */
-const messages = ['Website under construction ⚒️', "I'm working on it! ⛏️", 'Bugs are to be expected! 🚨 '];
-let index = 0;
-
-function changeMessage() {
-    document.querySelector('.construction-bar p').textContent = messages[index];
-    index = (index + 1) % messages.length;
-}
-
-setInterval(changeMessage, 5000);
-
