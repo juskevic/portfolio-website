5a46fcfd09f40fb2210d242db317a08a31b09d6f
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-01 18:54:29 +0200

    Removed comments

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,5 @@
-import '@/styles/globals.css'; // Adjust the path according to your project structure
+import '@/styles/globals.css';
 
-// This default export is required in a new `pages/_app.jsx` file.
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
