38992d58b9f60de244d2599ec8f7c98be029db70
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-04 19:37:02 +0200

    Added text to "about me". Added last.fm embed

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             flex flex-row justify-center space-x-16 p-4 text-1xl
             xl:flex xl:flex-row xl:justify-center xl:p-5 xl:text-3xl xl:space-x-96">
                 <h1 id="header-brand">makuyoshi.dev / about</h1>
-                <Navbar />
+                <Navbar/>
             </header>
         </>
     )
@@ -37,22 +37,31 @@ const Navbar = () => {
 const Main = () => {
     return (
         <>
-            <main>
-                <div className="flex flex-col justify-between pl-40 pt-16 text-2xl space-y-3">
-                    <h2>ABOUT ME</h2>
-                        <p>...About me text</p>
-                    <h2>TECH STACK</h2>
-                        <h3>Languages:</h3>
-                            <p>HTML, CSS, JavaScript</p>
-                        <h3>Frameworks:</h3>
-                            <p>Bootstrap, Tailwind CSS, React, Next.js</p>
-                        <h3>Other:</h3>
-                            <p>Git, Linux (Arch, Mint)</p>
-                    <h2>EXPERIENCE</h2>
-                    <h2>EDUCATION</h2>
+            <main className="flex">
+                <div className="about-me-font flex flex-col justify-between pl-40 pt-16 space-y-5 w-1/2">
+                    <h2 className="text-4xl">ABOUT ME</h2>
+                    <p className="about-me-font-2 text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                        laborum.</p>
+                    <h2 className="text-3xl">TECH STACK</h2>
+                    <h3 className="text-2xl">Languages:</h3>
+                    <p className="about-me-font-2 text-2xl">HTML, CSS, JavaScript</p>
+                    <h3 className="text-2xl">Frameworks:</h3>
+                    <p className="about-me-font-2 text-2xl">Bootstrap, Tailwind CSS, React, Next.js</p>
+                    <h3 className="text-2xl">Other:</h3>
+                    <p className="about-me-font-2 text-2xl">Git, Linux (Arch, Mint)</p>
+                    <h2 className="opacity-50 text-2xl">EXPERIENCE</h2>
+                    <h2 className="opacity-50 text-2xl">EDUCATION</h2>
                 </div>
-                <div>
-                    {/*Last.fm*/}
+                <div className="w-1/2 pl-32 pt-16">
+                    <a href="https://github.com/JeffreyCA/lastfm-recently-played-readme"><img
+                        src="https://lastfm-recently-played.vercel.app/api?user=makuyoshi&show_user=header&count=7&header_style=compact_stats_only"
+                        alt="last.fm scrobbles"
+                        className="border-2 border-border-silver rounded-2xl"/>
+                    </a>
                 </div>
             </main>
         </>
@@ -75,6 +84,9 @@ const AboutMePage = () => {
                 <link href="https://fonts.googleapis.com/css2?family=Exo:wght@300&display=swap" rel="stylesheet"/>
                 {/* eslint-disable-next-line @next/next/no-page-custom-font */}
                 <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet"/>
+                {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+                <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap"
+                      rel="stylesheet"/>
             </Head>
             <Header/>
             <Main/>
