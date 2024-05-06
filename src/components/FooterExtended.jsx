d9211941fa7ea4047f8a48b86a7d64edaef9165a
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-06 19:39:48 +0200

    Created FooterExtended.jsx

---
+++ src/components/FooterExtended.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FooterExtended = () => {
+    return (
+        <>
+            <footer>
+                <div className="flex flex-row justify-center absolute bottom p-4 w-full space-x-8 text-2xl">
+                    <a href="https://github.com/makuyoshi"><i className="bi bi-github"></i></a>
+                    <a href="https://twitter.com/makuyoshi_dev"><i className="bi bi-twitter-x"></i></a>
+                    <a className="opacity-50"><i className="bi bi-linkedin"></i></a>
+                    <a className="opacity-50"><i className="bi bi-mastodon"></i></a>
+                    <a className="opacity-50"><i className="bi bi-telegram"></i></a>
+                    <a href="https://www.last.fm/user/makuyoshi"><i className="fa fa-lastfm"></i></a>
+                    <a className="opacity-50"><i className="bi bi-xbox"></i></a>
+                    <button><i className="bi bi-envelope-at-fill"></i></button>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default FooterExtended;
