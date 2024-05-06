388223197290094101db86afee64d8c40a11a781
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-06 11:44:33 +0200

    Added opacity to unused socials

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -77,11 +77,11 @@ const Footer = () => {
                 <div className="flex flex-row justify-center absolute bottom p-4 w-full space-x-8 text-2xl">
                     <a href="https://github.com/makuyoshi"><i className="bi bi-github"></i></a>
                     <a href="https://twitter.com/makuyoshi_dev"><i className="bi bi-twitter-x"></i></a>
-                    <a href="#"><i className="bi bi-linkedin"></i></a>
-                    <a><i className="bi bi-mastodon"></i></a>
-                    <a><i className="bi bi-telegram"></i></a>
+                    <a className="opacity-50"><i className="bi bi-linkedin"></i></a>
+                    <a className="opacity-50"><i className="bi bi-mastodon"></i></a>
+                    <a className="opacity-50"><i className="bi bi-telegram"></i></a>
                     <a href="https://www.last.fm/user/makuyoshi"><i className="fa fa-lastfm"></i></a>
-                    <a><i className="bi bi-xbox"></i></a>
+                    <a className="opacity-50"><i className="bi bi-xbox"></i></a>
                     <button><i className="bi bi-envelope-at-fill"></i></button>
                 </div>
             </footer>
