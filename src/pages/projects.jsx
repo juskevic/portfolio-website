913ba7ad17be94c226a4389fbd9e59b615de96b0
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-06 19:41:06 +0200

    Used Footer component

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from "next/link";
+import Link from 'next/link';
+import Footer from '../components/Footer.jsx'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Header = () => {
@@ -67,22 +68,6 @@ const Main = () => {
                     </div>
                 </div>
             </main>
-        </>
-    )
-}
-
-const Footer = () => {
-    return (
-        <>
-            <footer>
-                <div className="flex flex-row justify-center absolute bottom p-4 w-full space-x-8 text-2xl">
-                    <a href="https://github.com/makuyoshi"><i className="bi bi-github"></i></a>
-                    <a href="https://twitter.com/makuyoshi_dev"><i className="bi bi-twitter-x"></i></a>
-                    <a className="opacity-50"><i className="bi bi-linkedin"></i></a>
-                    <a className="opacity-50"><i className="bi bi-mastodon"></i></a>
-                    <a className="opacity-50"><i className="bi bi-telegram"></i></a>
-                </div>
-            </footer>
         </>
     )
 }
