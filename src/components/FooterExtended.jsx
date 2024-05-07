1eca3f4589c7d23394fd37c5708016d500639707
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-07 12:50:28 +0200

    Removed unused social anchors

--- src/components/FooterExtended.jsx
+++ src/components/FooterExtended.jsx
@@ -9,10 +9,6 @@ const FooterExtended = () => {
                 <div className="flex flex-row justify-center absolute bottom p-4 w-full space-x-8 text-2xl">
                     <a href="https://github.com/makuyoshi"><i className="bi bi-github"></i></a>
                     <a href="https://twitter.com/makuyoshi_dev"><i className="bi bi-twitter-x"></i></a>
-                    <a className="opacity-50"><i className="bi bi-linkedin"></i></a>
-                    <a className="opacity-50"><i className="bi bi-mastodon"></i></a>
-                    <a className="opacity-50"><i className="bi bi-telegram"></i></a>
-                    <a className="opacity-50"><i className="bi bi-xbox"></i></a>
                     <button><i className="bi bi-envelope-at-fill"></i></button>
                 </div>
             </footer>
