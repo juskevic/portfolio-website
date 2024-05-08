31b78e205dabd34a2605748c1af71d570efd675a
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-08 13:12:17 +0200

    Added NextUIProvider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,11 @@
 import '@/styles/globals.css';
 
+import {NextUIProvider} from "@nextui-org/react";
+
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <NextUIProvider>
+            <Component {...pageProps} />
+        </NextUIProvider>
+    );
 }
