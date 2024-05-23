d735bb5c141336290d946503b0cb8d665a522c63
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-23 17:04:04 +0200

    Add FontAwesome free-brands-svg-icons and update Footer component
    Added "@fortawesome/free-brands-svg-icons" dependency to package.json and package-lock.json. Footer.jsx was updated to import and use FontAwesomeIcon and faBluesky from the added package. Also, enhanced visual appeal of anchor tags using classes for hover effect.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBluesky } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
         <>
             <footer>
                 <div className="flex flex-row justify-center p-4 w-full space-x-8 text-2xl bottom-0 sticky">
-                    <a href="https://github.com/makuyoshi"><i className="bi bi-github"></i></a>
-                    <a href="https://twitter.com/makuyoshi_dev"><i className="bi bi-twitter-x"></i></a>
+                    <a href="https://github.com/makuyoshi" className="hover:scale-110"><i className="bi bi-github"></i></a>
+                    <a href="https://twitter.com/makuyoshi_dev" className="hover:scale-110"><i className="bi bi-twitter-x"></i></a>
+                    <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faBluesky} /></a>
                 </div>
                 <div className="text-center mb-5">
                     <p>© {new Date().getFullYear()} makuyoshi.dev. All Rights Reserved.</p>
