53a73f83bdcea9a6061ec79aab5f110dcd693fe2
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-05-25 14:34:34 +0200

    Update social icons in Footer component
    Switched social media icons in the Footer component from bootstrap-icons to FontAwesome icons. In addition, removed a link to an unknown site while adding new links for LinkedIn and Telegram.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBluesky } from '@fortawesome/free-brands-svg-icons';
+import {faGithub, faLinkedinIn, faTelegram, faXTwitter} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
         <>
             <footer>
                 <div className="flex flex-row justify-center p-4 w-full space-x-8 text-2xl bottom-0 sticky">
-                    <a href="https://github.com/makuyoshi" className="hover:scale-110"><i className="bi bi-github"></i></a>
-                    <a href="https://twitter.com/makuyoshi_dev" className="hover:scale-110"><i className="bi bi-twitter-x"></i></a>
-                    <a href="https://bsky.app/profile/makuyoshi.bsky.socia" className="hover:scale-110"><FontAwesomeIcon icon={faBluesky} /></a>
+                    <a href="https://github.com/makuyoshi" className="hover:scale-110"><FontAwesomeIcon icon={faGithub} /></a>
+                    <a href="https://twitter.com/makuyoshi_dev" className="hover:scale-110"><FontAwesomeIcon icon={faXTwitter} /></a>
+                    <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                    <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faTelegram} /></a>
                 </div>
                 <div className="text-center mb-5">
                     <p>© {new Date().getFullYear()} makuyoshi.dev. All Rights Reserved.</p>
