040fa88dc18444b66f9541a9a27e1f062894ac5e
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-01 22:37:15 +0200

    Update Header component and global styles
    Implemented UI changes to the navbar components in Header.jsx and updated associated styling rules in globals.css. The changes include modifying text color and opacity for navbar items, and ensuring clearer active states with improved color contrast.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ const Header = () => {
     return (
         <>
             <header className="">
-                <nav className="flex flex-row justify-center pt-4 p-4 w-full text-2xl space-x-32 fixed">
+                <nav className="flex flex-row justify-center pt-4 p-4 w-full text-2xl space-x-32 fixed text-stone-400">
                     <div className="flex flex-row space-x-8 outline outline-1 rounded-2xl p-2 bg-black shadow-stone-300 shadow">
-                        <Link href="/#hello" activeClass="active" to="hello" spy={true} smooth={true} offset={-230} duration={500}>hello</Link>
-                        <Link href="/#about" activeClass="active" to="about" spy={true} smooth={true} offset={-100} duration={500}>about</Link>
-                        <Link href="/#projects" activeClass="active" to="projects" spy={true} smooth={true} offset={-100} duration={500}>projects</Link>
-                        <Link href="/#contact" activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={500}>contact</Link>
+                        <Link href="/#hello" activeClass="active" className="text-stone-600" to="hello" spy={true} smooth={true} offset={-230} duration={500}>hello</Link>
+                        <Link href="/#about" activeClass="active" className="text-stone-600" to="about" spy={true} smooth={true} offset={-100} duration={500}>about</Link>
+                        <Link href="/#projects" activeClass="active" className="text-stone-600" to="projects" spy={true} smooth={true} offset={-100} duration={500}>projects</Link>
+                        <Link href="/#contact" activeClass="active" className="text-stone-600" to="contact" spy={true} smooth={true} offset={-100} duration={500}>contact</Link>
                     </div>
-                    <Link href="#" className="outline outline-1 rounded-2xl p-2 bg-black shadow-stone-300 shadow hover:scale-105 hover:cursor-not-allowed">
+                    <Link href="#" className="outline outline-1 rounded-2xl p-2 bg-black shadow-stone-300 shadow hover:scale-105 hover:cursor-not-allowed opacity-50">
                         blog<i className="bi bi-arrow-right-short"></i>
                     </Link>
                 </nav>
