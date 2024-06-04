d5e53ee88acc30dc3e10beb1cb3575d8551651f0
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-04 13:10:01 +0200

    Replace Twitter icon with Stack Overflow icon
    The Twitter icon in the Socials component has been replaced with the Stack Overflow icon to match the actual intended link. The import statement has also been updated to reflect this change.

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faGithub, faLinkedinIn, faTelegram, faXTwitter} from "@fortawesome/free-brands-svg-icons";
+import {faGithub, faLinkedinIn, faStackOverflow, faTelegram} from "@fortawesome/free-brands-svg-icons";
 
 const Socials = () => {
     return (
@@ -8,7 +8,7 @@ const Socials = () => {
             <div>
                 <div className="flex flex-row justify-center p-4 w-full space-x-8 text-2xl bottom-0 sticky animate-fade-up animate-once animate-delay-200 animate-ease-out">
                     <a href="https://github.com/makuyoshi" className="hover:scale-110"><FontAwesomeIcon icon={faGithub} /></a>
-                    <a href="https://twitter.com/makuyoshi_dev" className="hover:scale-110"><FontAwesomeIcon icon={faXTwitter} /></a>
+                    <a href="https://twitter.com/makuyoshi_dev" className="hover:scale-110"><FontAwesomeIcon icon={faStackOverflow} /></a>
                     <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faLinkedinIn} /></a>
                     <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faTelegram} /></a>
                 </div>
