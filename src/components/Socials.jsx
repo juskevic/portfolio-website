10a6b556c936b6758d79405c668f850d08964107
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-04 15:15:21 +0200

    Update StackOverflow link in Socials component
    The link for StackOverflow in the Socials component was incorrectly directed to Twitter. It has now been corrected to redirect to the correct StackOverflow profile.

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -8,7 +8,7 @@ const Socials = () => {
             <div>
                 <div className="flex flex-row justify-center p-4 w-full space-x-8 text-2xl bottom-0 sticky animate-fade-up animate-once animate-delay-200 animate-ease-out">
                     <a href="https://github.com/makuyoshi" className="hover:scale-110"><FontAwesomeIcon icon={faGithub} /></a>
-                    <a href="https://twitter.com/makuyoshi_dev" className="hover:scale-110"><FontAwesomeIcon icon={faStackOverflow} /></a>
+                    <a href="https://stackoverflow.com/users/21579330/makuyoshi" className="hover:scale-110"><FontAwesomeIcon icon={faStackOverflow} /></a>
                     <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faLinkedinIn} /></a>
                     <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faTelegram} /></a>
                 </div>
