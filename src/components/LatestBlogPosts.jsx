7b51196ddbf7052b9e281144347dcaf61d6b9886
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-08 18:10:37 +0200

    Update header and link style in LatestBlogPosts
    The heading style of LatestBlogPosts component has been updated to a more interactive design. Furthermore, the arrow in the "Coming soon" section has been removed as it was providing redundant information. Now the header displays the text "Read my blog" with a corresponding link decorated with an appropriate CSS class.

--- src/components/LatestBlogPosts.jsx
+++ src/components/LatestBlogPosts.jsx
@@ -3,34 +3,38 @@ import React from 'react';
 const LatestBlogPosts = () => {
     return (
         <>
-            <h2 className="text-2xl pb-2
-            opacity-70">
-                Latest blog posts
-            </h2>
+            <div className="flex flex-row justify-between pb-2 text-2xl
+            opacity-80">
+                <h2 className="opacity-90">Read my blog</h2>
+                <a className="outline outline-1 rounded-2xl w-8 text-2xl pl-1 hover:text-gray-900 hover:bg-stone-50"
+                   href="#">
+                    <i className="bi bi-arrow-up-right"></i>
+                </a>
+            </div>
             <div className="flex justify-between items-center p-2
             opacity-90">
                 <h3 className="text-lg">31.12.2024</h3>
                 <div className="w-1/2
                 border border-stone-800 rounded-xl"></div>
-                <h3 className="text-lg text-right">Coming soon <i className="bi bi-arrow-up-right opacity-40"></i></h3>
+                <h3 className="text-lg text-right">Coming soon</h3>
             </div>
             <div className="flex justify-between items-center p-2 opacity-70">
                 <h3 className="text-lg">31.12.2024</h3>
                 <div className="w-1/2
                 border border-stone-800 rounded-xl"></div>
-                <h3 className="text-lg text-right">Coming soon <i className="bi bi-arrow-up-right opacity-40"></i></h3>
+                <h3 className="text-lg text-right">Coming soon</h3>
             </div>
             <div className="flex justify-between items-center p-2 opacity-50">
                 <h3 className="text-lg">31.12.2024</h3>
                 <div className="w-1/2
                 border border-stone-800 rounded-xl"></div>
-                <h3 className="text-lg text-right">Coming soon <i className="bi bi-arrow-up-right opacity-40"></i></h3>
+                <h3 className="text-lg text-right">Coming soon</h3>
             </div>
             <div className="flex justify-between items-center p-2 opacity-20">
                 <h3 className="text-lg">31.12.2024</h3>
                 <div className="w-1/2
                 border border-stone-800 rounded-xl"></div>
-                <h3 className="text-lg text-right">Coming soon <i className="bi bi-arrow-up-right opacity-40"></i></h3>
+                <h3 className="text-lg text-right">Coming soon</h3>
             </div>
         </>
     )
