4e897671e3131083df05d36be17f7595a98a83bf
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-15 21:52:39 +0200

    Update body background color and socials icons
    This commit updates the body background color in the global CSS styles, replacing the color variable with a direct hexadecimal color code. Also, in the 'Socials' component, the StackOverflow icon has been replaced with a mail icon to support direct contact via email. Furthermore, project presentation on the 'index' page has been rearranged for improved layout and readability.

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faGithub, faLinkedinIn, faStackOverflow, faTelegram} from "@fortawesome/free-brands-svg-icons";
+import {faGithub, faLinkedinIn, faTelegram} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
 const Socials = () => {
     return (
@@ -11,7 +12,7 @@ const Socials = () => {
                 xl:p-4 xl:w-full xl:space-x-8 xl:text-2xl xl:bottom-0 xl:sticky
                 animate-fade-up animate-once animate-delay-200 animate-ease-out">
                     <a href="https://github.com/makuyoshi" className="hover:scale-110"><FontAwesomeIcon icon={faGithub} /></a>
-                    <a href="https://stackoverflow.com/users/21579330/makuyoshi" className="hover:scale-110"><FontAwesomeIcon icon={faStackOverflow} /></a>
+                    <a href="mailto:contact@makuyoshi.dev" className="hover:scale-110"><FontAwesomeIcon icon={faEnvelope} /></a>
                     <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faLinkedinIn} /></a>
                     <a href="#" className="hover:scale-110"><FontAwesomeIcon icon={faTelegram} /></a>
                 </div>
