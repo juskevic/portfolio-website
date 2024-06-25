7358fa1422ac25258e4e785599c191e4bbb0472d
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-25 12:53:44 +0200

    Add NextUI to Footer component description
    In the Footer component, the Button import from NextUI has been added and the component description has been updated to reflect that the website has been built on top of Next.js, NextUI, React, and Tailwind CSS.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from '@nextui-org/react';
 
 const Footer = () => {
     return (
@@ -8,7 +9,7 @@ const Footer = () => {
                     p-3 text-sm bottom-0 opacity-70 space-y-2 pt-40 text-center
                     xl:p-4 xl:w-full xl:space-x-8 xl:text-lg xl:space-y-3 xl:pt-40">
                     <p className="text-center">
-                        This website has been built using <span className="text-gray-200">Next.js</span>, React & Tailwind CSS. <br />
+                        This website has been built on top of <span className="text-gray-200">Next.js</span>, NextUI, React & Tailwind CSS. <br />
                         Check out the <a href="https://github.com/makuyoshi/makuyoshi-web" className="text-gray-200 underline underline-offset-2">source-code on my GitHub page.</a>
                     </p>
                     <p className="text-center">
