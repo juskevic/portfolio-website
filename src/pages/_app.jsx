d9b892b7ace1e1dfe62dd93f770ecf833e41f6b0
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-06-25 12:53:32 +0200

    Integrate NextUIProvider into _app.jsx
    The NextUIProvider from the @nextui-org/react library has been imported and integrated into _app.jsx. This enhancement wraps the main application Component to ensure that NextUI's features are available throughout the app.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,14 @@
 import '@/styles/globals.css';
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { NextUIProvider } from '@nextui-org/react';
+
 config.autoAddCss = false;
+
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <NextUIProvider>
+            <Component {...pageProps} />
+        </NextUIProvider>
+    );
 }
