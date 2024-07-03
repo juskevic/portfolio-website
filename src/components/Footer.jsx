7a36cf8825fb03f1579d5b0f4214fe79c5b25712
makuyoshi <73663894+makuyoshi@users.noreply.github.com>
2024-07-03 19:17:39 +0200

    Update footer styling
    The footer's component style has been updated to modify brightness levels. In specific, a brightness modifier was added to the main footer tag and to the source-code link for improved readability and aesthetics.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import {Link} from "@nextui-org/react";
 const Footer = () => {
     return (
         <>
-            <footer className="flex flex-col justify-center p-5 space-y-2 pt-48">
+            <footer className="flex flex-col justify-center p-5 space-y-2 pt-48 brightness-50">
                 <p className="text-center">This website has been build on top of Next.js, using React & NextUI.</p>
-                <p className="text-center">You can check out the source-code <Link href="https://github.com/makuyoshi/makuyoshi-web" showAnchorIcon color="foreground" underline="always">here.</Link></p>
+                <p className="text-center">You can check out the source-code <Link href="https://github.com/makuyoshi/makuyoshi-web" showAnchorIcon color="foreground" underline="always" className="brightness-150">here.</Link></p>
                 <p className="text-center">© 2024 Max Yushkevich. All rights reserved.</p>
             </footer>
         </>
