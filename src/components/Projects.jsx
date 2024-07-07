33ec60897346998597fcda57f56add01351e152c
Max Yushkevich <73663894+makuyoshi@users.noreply.github.com>
2024-07-07 17:21:36 +0200

    Add Tabs components to Projects component
    The structure of the Projects component has been updated, by encapsulating the individual project rendering within Tabs components. This introduces a new navigation experience, making it easier for users to categorize and navigate through different project types.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ import {
     Divider,
     Link,
     Chip,
-    Button,
+    Button, Tabs, Tab,
 } from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
@@ -29,121 +29,131 @@ const Projects = (props) => {
 
     return (
         <div ref={forwardedRef}>
-            <div
-                className='flex flex-col xl:flex-row justify-center space-x-0 xl:space-x-10 xl:space-y-0 space-y-10 ${animationClass2}'>
-                {/*LEFT*/}
-                <div ref={forwardedRef} className={`flex flex-col space-y-10 ${animationClassRight}`}>
-                    <div>
-                        <Card className="max-w-[400px]">
-                            <CardHeader className="flex justify-between">
-                                <div className="space-x-4">
-                                    <Link className="text-2xl" href="#" underline="under"
-                                          color="foreground">Libro</Link>
-                                </div>
-                                <Chip color="warning" variant="flat">Archive</Chip>
-                                <div className="space-x-1">
-                                    <Button isIconOnly variant="ghost">
-                                        <FontAwesomeIcon icon={faGithub} size="lg"/>
-                                    </Button>
-                                </div>
-                            </CardHeader>
-                            <Divider/>
-                            <CardBody>
-                                <p>
-                                    Libro was a Discord bot crafted with Discord.js and Node.js, designed to
-                                    enhance your
-                                    server’s reading experience. It allowed users to search, share, and discuss
-                                    books
-                                    seamlessly within your Discord community
-                                </p>
-                            </CardBody>
-                        </Card>
+            <Tabs className={"pb-2"} size={"lg"} radius={"lg"} variant={"underlined"} disabledKeys={["testimonials", "contributions"]}>
+                <Tab key={"projects"} title="Projects">
+                    <div
+                        className='flex flex-col xl:flex-row justify-center space-x-0 xl:space-x-10 xl:space-y-0 space-y-10 ${animationClass2}'>
+                        {/*LEFT*/}
+                        <div ref={forwardedRef} className={`flex flex-col space-y-10 ${animationClassRight}`}>
+                            <div>
+                                <Card className="max-w-[400px]">
+                                    <CardHeader className="flex justify-between">
+                                        <div className="space-x-4">
+                                            <Link className="text-2xl" href="#" underline="under"
+                                                  color="foreground">Libro</Link>
+                                        </div>
+                                        <Chip color="warning" variant="flat">Archive</Chip>
+                                        <div className="space-x-1">
+                                            <Button isIconOnly variant="ghost">
+                                                <FontAwesomeIcon icon={faGithub} size="lg"/>
+                                            </Button>
+                                        </div>
+                                    </CardHeader>
+                                    <Divider/>
+                                    <CardBody>
+                                        <p>
+                                            Libro was a Discord bot crafted with Discord.js and Node.js, designed to
+                                            enhance your
+                                            server’s reading experience. It allowed users to search, share, and discuss
+                                            books
+                                            seamlessly within your Discord community
+                                        </p>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div>
+                                <Card className="max-w-[400px]">
+                                    <CardHeader className="flex justify-between">
+                                        <div className="space-x-4">
+                                            <Link className="text-2xl" href="#" underline="under"
+                                                  color="foreground">Highstorm</Link>
+                                        </div>
+                                        <Chip color="warning" variant="flat">Archive</Chip>
+                                        <div className="space-x-1">
+                                            <Button isIconOnly variant="ghost">
+                                                <FontAwesomeIcon icon={faGithub} size="lg"/>
+                                            </Button>
+                                            <Button isIconOnly variant="ghost">
+                                                <FontAwesomeIcon icon={faLink} size="lg"/>
+                                            </Button>
+                                        </div>
+                                    </CardHeader>
+                                    <Divider/>
+                                    <CardBody>
+                                        <p>
+                                            Highstorm was a web application that I developed in 2024. It was my
+                                            first attempt at
+                                            utilizing an API, and it was built using Bootstrap. The app showed basic
+                                            weather
+                                            information for a given location, including temperature, humidity,
+                                            clouds, sunset,
+                                            and sunrise.
+                                        </p>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                        </div>
+                        {/*MIDDLE*/}
+                        <div ref={forwardedRef} className={`flex flex-col ${animationClass}`}>
+                            <Card className="max-w-[400px]">
+                                <CardHeader className="flex justify-between">
+                                    <div className="space-x-4">
+                                        <Link className="text-2xl" href="#" underline="under" color="foreground">Portfolio
+                                            Website</Link>
+                                    </div>
+                                    <div className="space-x-1">
+                                        <Button isIconOnly variant="ghost">
+                                            <FontAwesomeIcon icon={faGithub} size="lg"/>
+                                        </Button>
+                                        <Button isIconOnly variant="ghost">
+                                            <FontAwesomeIcon icon={faLink} size="lg"/>
+                                        </Button>
+                                    </div>
+                                </CardHeader>
+                                <Divider/>
+                                <CardBody>
+                                    <p>
+                                        My portfolio website is the first front-end project I have undertaken. Initially
+                                        developed using basic HTML and CSS, it has since been enhanced with the use of
+                                        modern frameworks such as Next.js and React. It incorporates multiple JavaScript
+                                        libraries to achieve a contemporary look and feel. I am delighted with the
+                                        result
+                                        and continue to work on it to this day, despite having made over 500 commits.
+                                    </p>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        {/*RIGHT*/}
+                        <div ref={forwardedRef} className={`flex flex-col space-y-10 ${animationClassLeft}`}>
+                            <Card isHoverable className="max-w-[400px]">
+                                <CardHeader className="flex justify-between">
+                                    <div className="space-x-4">
+                                        <Link className="text-2xl" href="#" underline="under"
+                                              color="foreground">Výčetka</Link>
+                                    </div>
+                                    <div className="space-x-1">
+                                        <Button isIconOnly variant="ghost">
+                                            <FontAwesomeIcon icon={faGithub} size="lg"/>
+                                        </Button>
+                                        <Button isIconOnly variant="ghost">
+                                            <FontAwesomeIcon icon={faLink} size="lg"/>
+                                        </Button>
+                                    </div>
+                                </CardHeader>
+                                <Divider/>
+                                <CardBody>
+                                    <p>
+                                        This is an accounting tool build using Bootstrap that allows you to count the
+                                        amount of
+                                        banknotes and coins that are in your cash register.
+                                    </p>
+                                </CardBody>
+                            </Card>
+                        </div>
                     </div>
-                    <div>
-                        <Card className="max-w-[400px]">
-                            <CardHeader className="flex justify-between">
-                                <div className="space-x-4">
-                                    <Link className="text-2xl" href="#" underline="under" color="foreground">Highstorm</Link>
-                                </div>
-                                <Chip color="warning" variant="flat">Archive</Chip>
-                                <div className="space-x-1">
-                                    <Button isIconOnly variant="ghost">
-                                        <FontAwesomeIcon icon={faGithub} size="lg"/>
-                                    </Button>
-                                    <Button isIconOnly variant="ghost">
-                                        <FontAwesomeIcon icon={faLink} size="lg"/>
-                                    </Button>
-                                </div>
-                            </CardHeader>
-                            <Divider/>
-                            <CardBody>
-                                <p>
-                                    Highstorm was a web application that I developed in 2024. It was my
-                                    first attempt at
-                                    utilizing an API, and it was built using Bootstrap. The app showed basic
-                                    weather
-                                    information for a given location, including temperature, humidity,
-                                    clouds, sunset,
-                                    and sunrise.
-                                </p>
-                            </CardBody>
-                        </Card>
-                    </div>
-                </div>
-                {/*MIDDLE*/}
-                <div ref={forwardedRef} className={`flex flex-col ${animationClass}`}>
-                    <Card className="max-w-[400px]">
-                        <CardHeader className="flex justify-between">
-                            <div className="space-x-4">
-                                <Link className="text-2xl" href="#" underline="under" color="foreground">Portfolio Website</Link>
-                            </div>
-                            <div className="space-x-1">
-                                <Button isIconOnly variant="ghost">
-                                    <FontAwesomeIcon icon={faGithub} size="lg"/>
-                                </Button>
-                                <Button isIconOnly variant="ghost">
-                                    <FontAwesomeIcon icon={faLink} size="lg"/>
-                                </Button>
-                            </div>
-                        </CardHeader>
-                        <Divider/>
-                        <CardBody>
-                            <p>
-                                My portfolio website is the first front-end project I have undertaken. Initially
-                                developed using basic HTML and CSS, it has since been enhanced with the use of
-                                modern frameworks such as Next.js and React. It incorporates multiple JavaScript
-                                libraries to achieve a contemporary look and feel. I am delighted with the result
-                                and continue to work on it to this day, despite having made over 500 commits.
-                            </p>
-                        </CardBody>
-                    </Card>
-                </div>
-                {/*RIGHT*/}
-                <div ref={forwardedRef} className={`flex flex-col space-y-10 ${animationClassLeft}`}>
-                    <Card isHoverable className="max-w-[400px]">
-                        <CardHeader className="flex justify-between">
-                            <div className="space-x-4">
-                                <Link className="text-2xl" href="#" underline="under" color="foreground">Výčetka</Link>
-                            </div>
-                            <div className="space-x-1">
-                                <Button isIconOnly variant="ghost">
-                                    <FontAwesomeIcon icon={faGithub} size="lg"/>
-                                </Button>
-                                <Button isIconOnly variant="ghost">
-                                    <FontAwesomeIcon icon={faLink} size="lg"/>
-                                </Button>
-                            </div>
-                        </CardHeader>
-                        <Divider/>
-                        <CardBody>
-                            <p>
-                                This is an accounting tool build using Bootstrap that allows you to count the amount of
-                                banknotes and coins that are in your cash register.
-                            </p>
-                        </CardBody>
-                    </Card>
-                </div>
-            </div>
+                </Tab>
+                <Tab key={"contributions"} title={"Contributions"}></Tab>
+            </Tabs>
         </div>
     );
 };
